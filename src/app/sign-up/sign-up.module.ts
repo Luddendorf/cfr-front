6d03d72d7da39c89ee56a44467d6bc9456e61537
Luddendorf <luddendorf2@yahoo.com>
2024-02-29 17:14:09 +0200

    Reactive forms - beginning

--- src/app/sign-up/sign-up.module.ts
+++ src/app/sign-up/sign-up.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SharedModule } from '../common/shared.module';
-// import { ButtonComponent } from '../common/components/button/button.component';
 
 @NgModule({
   declarations: [SignUpComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     SharedModule
-    //ButtonComponent
   ]
 })
 export class SignUpModule { }
--- src/app/sign-up/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up/sign-up.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'cfr-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  // name = new FormControl('');
+  nameControl: AbstractControl;
+  planetControl: AbstractControl;
+  galaxyControl: AbstractControl;
+  passwordControl: AbstractControl;
+  
+  signUpForm = this.fb.group({
+    name: ['Bob', Validators.required],
+    planet: [''],
+    galaxy: [''],
+    password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9!@#$%^&*]{6,16}$')]]
+    // address: this.fb.group({
+    //   street: [''],
+    //   city: ['']
+    // })
+  });
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.nameControl = this.signUpForm.get('name');
+    this.planetControl = this.signUpForm.get('planet');
+    this.galaxyControl = this.signUpForm.get('galaxy');
+    this.passwordControl = this.signUpForm.get('password');
+  }
+//   this.teamForm = this.formBuilder.group({
+//     teamName: ['', Validators.required ],
+//     teamManager: '',
+//     teamDept: this.formBuilder.group(new Department()),
+//     employees: this.formBuilder.array([]) 
+//  });
+ 
+  onSignUpSubmit(): void {
+    console.log(this.signUpForm.valid);
+    console.log(this.signUpForm);
+  }
 }
