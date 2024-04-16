6cc9f02bbf1f6c0e0389e56c5228b6bc368eb597
Luddendorf <luddendorf2@yahoo.com>
2024-04-16 18:33:24 +0300

    Clear button in input

--- src/app/main/shared/input/input.component.ts
+++ src/app/main/shared/input/input.component.ts
@@ -1,33 +1,84 @@
-import { Component, Input } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'cfr-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting: InputComponent
+    }
+  ]
 })
 export class InputComponent implements ControlValueAccessor  {
+  userInput: string = '';
+  touched: boolean = false;
+  disabled: boolean = false;
   // errorMessage: string = '';
+
   @Input()
   data = {
     placeholder: 'Enter Your email',
     error: {
-      required: 'Email is required.',
-      pattern: 'Email is not valid',
-      taken: 'Email is already in use.'
+      required: null,
+      // pattern: 'Email is not valid',
+      // taken: 'Email is already in use.'
     }
   };
 
-  get errorMessage(): string {
+  @ViewChild('inputElement')
+	private inputElement = {} as ElementRef;
+
+  onChange = (unserInput: string) => {};
+
+  onTouched: Function = (touched: boolean) => {
+    this.touched = touched;
+  };
+
+  get errorMessage(): string | null {
     return Object.values(this.data.error).filter(e => {
       return e !== undefined && e !== null && e !== ''
     })[0];
   }
 
-  takeInput(event: KeyboardEvent): void {
+  onInput(event: KeyboardEvent): void {
+    this.markAsTouched();
+    if (this.disabled) {
+      return;
+    }
     const targetElement = event.target as HTMLInputElement;
     const userInput: string = targetElement.value;
-    console.log(userInput);
-    
+    this.userInput = userInput;
+    this.onChange(this.userInput);
+  }
+
+  writeValue(userInput: string): void {
+    this.userInput = userInput;
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.touched = true;
+      this.onTouched(this.touched);
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+    this.inputElement.nativeElement.disabled = disabled;
+    if (disabled) {
+      this.inputElement.nativeElement.value = '';
+    }
   }
 }
